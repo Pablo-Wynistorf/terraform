77b7c351c4e3a981f343a485888cfecbac50eb1b
Pablo-Wynistorf <p.wynistorf@gmail.com>
2023-12-10 22:40:38 +0100

    Added variables at ACM Cert creation

--- aws/create-acm-cert/create-acm-cert.tf
+++ aws/create-acm-cert/create-acm-cert.tf
@@ -1,35 +1,28 @@
 provider "aws" {
   region = var.var-region
 }
+
 variable "var-region" {
   description = "In what region do you want the infrastructure?"
 }
 
-
-resource "aws_acm_certificate" "domain_request_1" {
-  domain_name       = "*.onedns.ch"
-  validation_method = "EMAIL"
+variable "var-hostname" {
+  description = "Enter the hostname you want to create a certificate for"
 }
 
-resource "aws_acm_certificate" "domain_request_2" {
-  domain_name       = "onedns.ch"
-  validation_method = "EMAIL"
+variable "var-method" {
+  description = "Enter the validation method. (EMAIL or DNS)"
 }
 
-resource "aws_acm_certificate" "domain_request_3" {
-  domain_name       = "*.bla.ch"
-  validation_method = "DNS"
-}
-
-resource "aws_acm_certificate" "domain_request_4" {
-  domain_name       = "bla.ch"
-  validation_method = "DNS"
+resource "aws_acm_certificate" "domain_request" {
+  domain_name       = var.var-hostname
+  validation_method = var.var-method
 }
 
 #output the dns validation records (in this case for request 4)
 output "acm_certificate_validation_dns_records_name" {
-  value = aws_acm_certificate.domain_request_4.domain_validation_options[*].resource_record_name
+  value = aws_acm_certificate.domain_request.domain_validation_options[*].resource_record_name
 }
 output "acm_certificate_validation_dns_records_value" {
-  value = aws_acm_certificate.domain_request_4.domain_validation_options[*].resource_record_value
+  value = aws_acm_certificate.domain_request.domain_validation_options[*].resource_record_value
 }
